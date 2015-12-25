b196178544ad5a7cf12cb992f30f0bfce78f34c9
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-25 14:00:08 +0530

    Quora Card UI help complete

--- app/static/js/quora-widget.js
+++ app/static/js/quora-widget.js
@@ -1,4 +1,4 @@
-function quora_widget(url, element_id) {
+function quora_widget(url, element) {
     var xmlhttp;
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -11,7 +11,8 @@ function quora_widget(url, element_id) {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
             if(xmlhttp.status == 200){
-                document.getElementById(element_id).innerHTML = xmlhttp.responseText;
+                console.log(element);
+                element.innerHTML = xmlhttp.responseText;
             }
             else if(xmlhttp.status == 400) {
                 alert('There was an error 400')
@@ -21,6 +22,17 @@ function quora_widget(url, element_id) {
             }
         }
     }
+
     xmlhttp.open("GET", "http://codeville.org.in/quora/process?url=" + url, true);
     xmlhttp.send();
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+    var quora_profile_elements = document.querySelectorAll("[quora-profile]");
+    for(var i = 0; i < quora_profile_elements.length; i++) {
+        var quora_profile_element = quora_profile_elements[i];
+        var url = quora_profile_element.getAttribute('quora-profile');
+
+        quora_widget(url, quora_profile_element);
+    }
+}, false);
--- app/static/js/quora.js
+++ app/static/js/quora.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $('#quora_widget_button').click(function() {
-        quora_widget($('#quora_widget_url').val(), 'quora_widget_preview');
+        var url = $('#quora_widget_url').val();
+        $('#quora_widget_preview').addClass('loading');
+        $('#steps').addClass('loading');
+
+        $.get('http://codeville.org.in/quora/process', {url: url}, function(result) {
+            $('#quora_widget_preview').html(result);
+            $('#steps').show();
+            $('#steps pre').html('\
+&lt;<span class="tag">div</span> type=<span class="str">"card"</span> quora-profile=<span class="str">"'+ url +'"</span>&gt;&lt;/<span class="tag">div</span>&gt;\n\
+&lt;<span class="tag">script</span> src=<span class="str">"/static/js/quora-widget.js"</span>&gt;&lt;/<span class="tag">script</span>&gt;\
+');
+        }).always(function(){
+            $('#quora_widget_preview').removeClass('loading');
+            $('#steps').removeClass('loading');
+        });
     });
 });

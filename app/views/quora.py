0139487a7fca5e835dc3b3e4783a439016a6954b
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-28 23:54:50 +0530

    Removing unused imports

--- app/views/quora.py
+++ app/views/quora.py
@@ -1,12 +1,5 @@
-import app
-import requests
 from flask import Blueprint
-from flask import render_template, request
-from flask.ext.login import login_required
-
-from bs4 import BeautifulSoup
-
-from app.exceptions import *
+from flask import render_template
 
 mod = Blueprint('quora', __name__)
 
